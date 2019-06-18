8797a54b0c0198bbe1901db93df42daf17929699
zackery <zpainter.1234@gmail.com>
2019-06-17 21:54:01 -0400

    Add comments

--- FireTVConnect/Test.py
+++ FireTVConnect/Test.py
@@ -1,13 +1,20 @@
-input("Press Enter")
-import FireTVConnect as firetv
-import sys
-tv=firetv.Connector("192.168.1.73")
-tv.Reset()
-tv.Connect()
+""" File Name: Test.py
+    Creator: Zackery Painter
+    Date Created: 6/16/2019
+    Date Modified: 6/17/2019
+    Description: This is a test program for the FireTVConnect module. 
+"""
+
+import FireTVConnect as firetv # Import the FireTVConnector
+import sys # Import sys module
+# Create a fire tv connector address
+tv=firetv.Connector("IP ADDRESS") # Put in your FireTV's address here. 
+tv.Reset() # Reset and clean up any previous processes 
+tv.Connect() # Connect to the FireTV
 try:
     while True:
-        tv.KeyEvent("Play/Pause")
+        tv.KeyEvent("Play/Pause") # Send the key event to Play or Pause
         input("Press Enter to play/pause")
 finally:
-    tv.Reset()
+    tv.Reset() # Disconnect and kill the server
     sys.exit(0)
